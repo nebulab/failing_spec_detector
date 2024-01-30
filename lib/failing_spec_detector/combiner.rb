881be2ecbce849e6dc0290c0053b6ddf59a576c8
safa <aballaghsafa@gmail.com>
2024-01-30 14:55:31 +0100

    Add HTML output

--- lib/failing_spec_detector/combiner.rb
+++ lib/failing_spec_detector/combiner.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'erb'
+
 module FailingSpecDetector
   # Combiner class to combine and group the failures by exception
   class Combiner
@@ -7,6 +9,7 @@ module FailingSpecDetector
       @exceptions = exceptions
       @failures = failures
       @filename = 'failing_specs_detector_log.txt'
+      @html_filename = 'failing_specs_detector_log.html'
     end
 
     def combine
@@ -24,6 +27,13 @@ module FailingSpecDetector
         File.write(@filename, "\n\n\n", mode: 'a')
       end
       File.write(@filename, '----------------------------------------------------------------', mode: 'a')
+    end
+
+    def combine_html
+      return if @exceptions.empty?
+
+      template = ERB.new(File.read(File.join(File.dirname(__FILE__), 'views', 'template.erb')))
+      File.write(@html_filename, template.result(binding))
     end
   end
 end
